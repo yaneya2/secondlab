edc0a7fdbb9e7efcf5db79aab21f09eb04c60e6c
Vasya <vasya.shilin.smol@gmail.com>
2026-04-19 19:05:15 +0300

    Комментарий:

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
-#include <iostream>
-
 #include "classes/headers/UI.h"
 #include <windows.h>
-#include "classes/headers/MutableArraySequence.h"
-#include  "classes/headers/Sequence.h"
 
 int main() {
     system("chcp 65001");
